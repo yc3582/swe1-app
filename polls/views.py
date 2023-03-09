b53371e0744bdb60fbeebaefc973d945393b43ea
yc3582 <yc3582@nyu.edu>
2023-03-08 21:32:05 -0500

    Update views.py

--- polls/views.py
+++ polls/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponseRedirect
 from django.views import generic
 from .models import Choice, Question
 
+
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
@@ -11,14 +12,17 @@ class IndexView(generic.ListView):
     def get_queryset(self):
         return Question.objects.order_by("-pub_date")[:5]
 
+
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
 
+
 class ResultsView(generic.DetailView):
     model = Question
     template_name = "polls/results.html"
-    
+
+
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
