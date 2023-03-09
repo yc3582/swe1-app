ba76af1a3b03c454650a004d42c958022d322bff
yc3582 <yc3582@nyu.edu>
2023-03-08 21:22:57 -0500

    update unused

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
-from django.http import HttpResponse, Http404, HttpResponseRedirect
-from django.template import loader
+from django.http import HttpResponseRedirect
 from django.views import generic
 from .models import Choice, Question
 
@@ -12,11 +11,9 @@ def index(request):
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
     #return HttpResponse(template.render(context, request))
-    
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     output = ', '.join([q.question_text for q in latest_question_list])
     return HttpResponse(output)
-    
     #return HttpResponse("Hello, world. You're at the polls index.")
 """
 
@@ -34,13 +31,11 @@ class IndexView(generic.ListView):
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
-    
     try:
         question = Question.objects.get(pk=question_id)
     except Question.DoesNotExist:
         raise Http404("Question does not exist")
     return render(request, 'polls/detail.html', {'question': question})
-    
     #return HttpResponse("You're looking at question %s." % question_id)
 """
 
